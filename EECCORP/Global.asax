14cd54866ff186447d74e74c2e3a1ea3ead8926e
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2017-11-23 08:40:28 -0700

    Add project files.

---
+++ EECCORP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EECCORP.MvcApplication" Language="C#" %>
---
+++ EECCORP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EECCORP.Startup))]
+namespace EECCORP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
